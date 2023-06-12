62f55393f26c03a3bdec08e685c83682a127b9da
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-06-12 14:28:19 +0200

    Add files via upload

---
+++ PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Models;
+
+namespace PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("ListOrders")]
+        public IActionResult Index()
+        {
+            List<Order> Orders = StaticOrder.Orders;
+            return View(Orders);
+        }
+
+        [Route("details/{id?}")]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            Order order = StaticOrder.Orders.FirstOrDefault(x => x.Id == id);
+
+            if (order == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(order);
+        }
+
+        [Route("Json")]
+        public IActionResult GetJson()
+        {
+            Order order = new Order()
+            {
+                Id = 3,
+                Name = "Capriciosa"
+            };
+
+            return new JsonResult(order);
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ PizzaApp/StaticOrder.cs
@@ -0,0 +1,21 @@
+﻿using PizzaApp.Models;
+
+namespace PizzaApp
+{
+    public static class StaticOrder
+    {
+        public static List<Order> Orders = new List<Order>()
+        {
+            new Order()
+            {
+                Id = 1,
+                Name = "Capri"
+            },
+            new Order()
+            {
+                Id = 2,
+                Name = "Pepperoni"
+            }
+        };
+    }
+}
