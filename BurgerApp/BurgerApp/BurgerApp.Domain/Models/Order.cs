6095972e3f96380f0280f1ebba692668d90a2090
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-06-26 15:32:42 +0200

    Add files via upload

---
+++ BurgerApp/BurgerApp/BurgerApp.Domain/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerApp.Domain.Models
+{
+    public class Order
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public bool IsDelivered { get; set; }
+        public List<Burger> Burgers { get; set; } = new List<Burger>();
+        public Location Location { get; set; }
+    }
+}
